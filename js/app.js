9b7583c0cdf8a7c78ccb18c297fc710e197217d4
Tales Wallace Souza <tales.wallace@gmail.com>
2021-01-04 02:27:27 -0300

    English, text, date corrections

--- js/app.js
+++ js/app.js
@@ -365,3 +365,17 @@ app.factory('TransactionsResource', function ($resource) {
 
     });
 });
+
+app.directive('dateFix', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModel) {
+            element.on('change', function() {
+                scope.$apply(function () {
+                    ngModel.$setViewValue(element.val());
+                });         
+            });
+        }
+    };
+});
